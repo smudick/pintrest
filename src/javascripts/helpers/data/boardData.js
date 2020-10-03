79298337a31fb5d7ab43d0e58187463cda87a235
Sam Mudick <smudick@gmail.com>
2020-10-03 15:46:31 -0500

    can delete pins and boards

--- src/javascripts/helpers/data/boardData.js
+++ src/javascripts/helpers/data/boardData.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import apiKeys from '../apiKeys.json';
+import pinData from './pinData';
 
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
@@ -20,4 +21,27 @@ const getAllBoards = () => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
-export default { getAllBoards };
+const getSingleBoard = (boardUid) => new Promise((resolve, reject) => axios
+  .get(`${baseUrl}/boards.json?orderBy="uid"&equalTo="${boardUid}"`)
+  .then((response) => {
+    const board = Object.values(response.data);
+    const thisBoard = board[0];
+    resolve(thisBoard);
+  }).catch((error) => reject(error)));
+
+const deleteBoard = (boardUid) => {
+  pinData
+    .getBoardPins(boardUid)
+    .then((response) => {
+      response.forEach((item) => {
+        pinData.deletePin(item.uid);
+      });
+    })
+    .then(() => {
+      getSingleBoard(boardUid).then((response) => {
+        axios.delete(`${baseUrl}/boards/${response.uid}.json`);
+      });
+    });
+};
+
+export default { getAllBoards, deleteBoard };
