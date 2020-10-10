53c960b96be69f01fdf6b7247540ea6e5b8a7f47
Sam Mudick <smudick@gmail.com>
2020-10-09 21:31:39 -0500

    adjusted layout

--- src/javascripts/components/cards/boardCards.js
+++ src/javascripts/components/cards/boardCards.js
@@ -2,7 +2,7 @@ import boardData from '../../helpers/data/boardData';
 
 const boardMaker = (boardObject) => {
   const domString = `<div class="card board" style="width: 18rem;" id="${boardObject.uid}">
-                     <img src="${boardObject.imageUrl}" class="card-img-top" alt="${boardObject.name}">
+                     <img src="${boardObject.imageUrl}" class="card-img-top" style="width:18rem; height:12rem;" alt="${boardObject.name}">
                   <div class="card-body">
                     <h5 class="card-title">${boardObject.name}</h5>
                     <a href='#' id="${boardObject.uid}" class="btn btn-info view-pins"> View Pins</a>
